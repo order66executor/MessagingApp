862af4f4a0ee3ba2cad9d6d727ce0d3d53e2f110
Kovács Erik Marcell <61799696+order66executor@users.noreply.github.com>
2026-07-22 23:44:00 +0200

    misc: began work on ack handling

---
+++ src/Messaging.Shared/Services/PendingAckTracker.cs
@@ -0,0 +1,44 @@
+using System.Collections.Concurrent;
+
+using Messaging.Shared.Models;
+
+namespace Messaging.Shared.Services;
+
+public class PendingAckTracker {
+    private readonly ConcurrentDictionary<(int, StringIdentifier), TaskCompletionSource<bool>> pendingMessages;
+    private readonly CancellationToken ct;
+
+    private static readonly TimeSpan waitLength = TimeSpan.FromSeconds(5);
+
+    public PendingAckTracker(CancellationToken ct) {
+        pendingMessages = new();
+        this.ct = ct;
+    }
+
+    //waits for waitLength for someone to call complete on the key
+    public async Task<bool> RegisterWait((int Id, StringIdentifier Target) key) {
+        // create tcs to complete
+        TaskCompletionSource<bool> tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
+        pendingMessages.TryAdd(key, tcs);
+        
+        //wait for waitLength or task completion
+        Task result = await Task.WhenAny(tcs.Task, Task.Delay(waitLength, ct));
+        if (result != tcs.Task)
+            tcs.TrySetResult(false);
+        pendingMessages.TryRemove(key, out _);
+
+        //return if task completed or timed out
+        return tcs.Task.Result;
+    }
+
+    //complete waiting for key
+    public void Complete((int Id, StringIdentifier Target) key) {
+        if(!pendingMessages.TryGetValue(key, out var result))
+            Console.WriteLine("Ack wait cannot be completed, no such dict entry");
+        
+        if (result is null) return;
+        result.TrySetResult(true);
+    }
+
+
+}
