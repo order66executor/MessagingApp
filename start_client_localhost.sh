e623c01ad27063b4af002c6b069b8f3f3349f73e
Erik Kovacs <61799696+order66executor@users.noreply.github.com>
2026-07-24 13:02:34 +0200

    Removed accidentally included shell scripts

--- start_client_localhost.sh
+++
@@ -1,1 +0,0 @@
-dotnet run --project /home/erik/Documents/Code/MessagingApp/src/Messaging.Client/Messaging.Client.csproj -- ::1 5000
--- start_server.sh
+++
@@ -1,1 +0,0 @@
-dotnet run --project /home/erik/Documents/Code/MessagingApp/src/Messaging.Server/Messaging.Server.csproj -- 5000
